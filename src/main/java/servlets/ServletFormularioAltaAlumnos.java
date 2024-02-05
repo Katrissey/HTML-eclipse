58b49b277c4bcd19da05e2b0b9888d2f4229311a
Katrissey <correokt@gmail.com>
2024-02-05 20:14:53 +0100

    Comparativa mío profe alumnos html y toma de apuntes

--- src/main/java/servlets/ServletFormularioAltaAlumnos.java
+++ src/main/java/servlets/ServletFormularioAltaAlumnos.java
@@ -10,74 +10,82 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class FormularioAltaAlumnos
  */
 public class ServletFormularioAltaAlumnos extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	List<AlumnosHtml> alumnosHtml = new ArrayList<AlumnosHtml>();
 
-    /**
-     * Default constructor. 
-     */
-    public ServletFormularioAltaAlumnos() {
-        // TODO Auto-generated constructor stub
-    }
+	List<AlumnosHtml> alumnosHtml;
 
-    
-    
+	/**
+	 * Default constructor.
+	 */
+	public ServletFormularioAltaAlumnos() {
+		System.out.println("Este es el constructor del Servlet de AlumnosHtml");
+		// Para comprobar que pasa por aquí, es una traza.
+	}
+
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
-		//super.init(config); no es indispensable, sale por defecto al sobreescribir est método
-		
-	
-		
-	}
+		// super.init(config); no es indispensable, sale por defecto al sobreescribir
+		// est método
+		alumnosHtml = new ArrayList<AlumnosHtml>(); // Inicializamos aquí la lista de alumnosHtml
+		// para que se cree al iniciar el servidor
 
-
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-				
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		/*
+		 * Para crear sesión de usuario hay que hacer los iguiente y, a la hora de importar,
+		 * hacerlo con "import jakarta.servlet.http.HttpSession;", ya que es el tipo de objeto
+		 * que devuelve el "request.getSession();
+		 */
+		HttpSession session = request.getSession();
+		
 		if (request.getParameter("Nombre") != null && request.getParameter("Apellidos") != null) {
 			String nombre = request.getParameter("Nombre");
 			String apellidos = request.getParameter("Apellidos");
 			String sexo = request.getParameter("Sexo");
 			String asignaturaFavorita = request.getParameter("Escoge_tu_asignatura_favorita");
 			String infoAdicionalAlumno = request.getParameter("Información_adicional_del_alumno");
-			
-			AlumnosHtml alumno = new AlumnosHtml (nombre, apellidos, sexo, asignaturaFavorita, infoAdicionalAlumno);
-			
+
+			AlumnosHtml alumno = new AlumnosHtml(nombre, apellidos, sexo, asignaturaFavorita, infoAdicionalAlumno);
+
 			alumnosHtml.add(alumno);
-			
+
 			response.getWriter().append("Bienvenido " + alumno.getNombre() + alumno.getApellidos());
-			
+
 			response.setContentType("text/html");
-			
+
 			for (AlumnosHtml alumnoHtml : alumnosHtml) {
-				
-				response.getWriter().append("<H2>La lista actual de alumnos registrados es: " + "\n" + alumnoHtml.getNombre() + "  " + alumnoHtml.getApellidos() + "\n<H2>");
-			
+
+				response.getWriter().append("<H2>La lista actual de alumnos registrados es: " + "\n"
+						+ alumnoHtml.getNombre() + "  " + alumnoHtml.getApellidos() + "\n<H2>");
+
 			}
-			
+
 		} else {
 			response.getWriter().append("Faltan por rellenar campos");
 		}
-		
+
 	}
 
 }
--- src/main/webapp/Ejercicios/IntentosMios/js/TablaAlumnos.jsp
+++ src/main/webapp/Ejercicios/IntentosMios/js/TablaAlumnos.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=utf-8"
-    pageEncoding="utf-8"%>
+    pageEncoding="utf-8" import="java.util.List,ejercicios.AlumnosHtml"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +7,21 @@
 <title>Insert title here</title>
 </head>
 <body>
+<h1>LISTA DE ALUMNOS</h1>
+<%
+//Creamos una variable Lista de tipo AlumnoHtml para que guarde la lista de alumnos
+//obtenida en el request
+//Realizamos un cast para decirle a java que el objeto no es de tipo object
+// sino de tipo AlumnosHtml
+List<AlumnosHtml> alumnos = (List<AlumnosHtml>)request.getAttribute("");
+//Ahora haremos un "sysout" para comprobar que está pasando por aquí y, de paso,
+//el número de alumnos que se han insertado hasta este momento.
+//Recordar que esta información se verá sólo por consola.
+System.out.println ("El número de alumnos insertados es " + alumnos.size());
+%>
+
 	<table>
-		<caption>Lista de Alumnos</caption>
+		
 		<tr>
 			<tr>Nombre</tr>
 			<tr>Apellidos</tr>
@@ -16,7 +29,12 @@
 			<tr>Asignatura favorita</tr>
 			<tr>Información adicional del alumno</tr>
 		</tr>
+		<%
+		for (AlumnosHtml alumno : alumnos){
+		%>
+			
 	
+	<% } %>
 	
 	</table>
 
